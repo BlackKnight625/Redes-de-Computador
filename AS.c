b45ae4ef2a015d162c44db97a4e37f18392d5f90
miguel-lourenco <miguel.lourenco@hotmail.com>
2020-10-29 09:16:27 +0000

    made changes in Makefile and started AS

--- AS.c
+++ AS.c
@@ -1,1 +1,66 @@
 //AS
+
+#include "libs/helper.h"
+
+#define SIZE 128
+#define max(A, B) ((A) >= (B) ? (A) : (B))
+
+char *asport;
+int verboseMode = FALSE;
+
+void processCommands() {
+    int counter, maxfd;
+    int fd = 0;
+    fd_set readfds;
+    struct timeval timeout;
+    User user;
+
+    Sock *sfd = newUDPServer(pdport);
+
+    while (1) {
+        FD_ZERO(&readfds);
+        FD_SET(fd, &readfds);
+        FD_SET(sfd->fd, &readfds);
+
+        maxfd = sfd->fd;
+        
+        //timeout.tv_sec = 5;
+        //timeout.tv_usec = 0;
+        
+        counter = select(maxfd+1, &readfds, NULL, NULL, (struct timeval *)NULL);
+        
+        if (counter <= 0) {
+            printf("Reached timeout.\n");
+        }
+
+        // insert other FD_ISSET conditions here
+    }
+}
+
+int main(int argc, char *argv[]) {
+    Map *myMap = newMap();
+
+    if (argc <= 1) {
+        fprintf(stderr, "not enough arguments\n");
+    } else {
+        for (int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "-v") == 0) {
+                verboseMode = TRUE;
+                continue;
+            }
+
+            put(myMap, argv[i], argv[i+1]);
+            i++;
+        }
+    }
+
+    if ((asport = get(myMap, "-p")) == NULL) { asport = AS_PORT; }
+
+    processCommands();
+
+    printf("terminating...\n");
+
+    delete(myMap);
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -1,5 +1,17 @@
-all: libs/helper.o libs/udp.o libs/tcp.o pd.o
+all: libs/helper.o libs/udp.o libs/tcp.o pd.o fs.o as.o user.o
 	gcc -g -o pd libs/helper.o libs/udp.o libs/tcp.o pd.o
+	gcc -g -o fs libs/helper.o libs/udp.o libs/tcp.o fs.o
+	gcc -g -o as libs/helper.o libs/udp.o libs/tcp.o as.o
+	gcc -g -o user libs/helper.o libs/udp.o libs/tcp.o user.o
+
+fs.o: User.c libs/helper.h libs/udp.h libs/tcp.h
+	gcc -g -o user.o -c User.c
+
+fs.o: AS.c libs/helper.h libs/udp.h libs/tcp.h
+	gcc -g -o as.o -c AS.c
+
+fs.o: FS.c libs/helper.h libs/udp.h libs/tcp.h
+	gcc -g -o fs.o -c FS.c
 
 pd.o: PD.c libs/helper.h libs/udp.h libs/tcp.h
 	gcc -g -o pd.o -c PD.c
