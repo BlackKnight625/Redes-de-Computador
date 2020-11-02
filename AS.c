898df08149042bae4863e9240d9c6ea2ca34ca59
miguel-lourenco <miguel.lourenco@hotmail.com>
2020-11-02 17:02:33 +0000

    started working on AS

--- AS.c
+++ AS.c
@@ -8,8 +8,31 @@
 char *asport;
 int verboseMode = FALSE;
 
-void getUDPrequests() {
-    
+void getUDPrequests(Sock *sfd) {
+    char buffer[SIZE];
+    char op[SIZE], uid[UID_LENGTH+1], pw[PASS_LENGTH+1];
+    char pdip[SIZE], pdport[SIZE];
+
+    memset(buffer, 0, SIZE);
+
+    receiveMessage(sfd, buffer, SIZE);
+
+    sscanf(buffer, "%s %s %s %s %s", op, uid, pw, pdip, pdport);
+
+    int words = getWords(buffer);
+    memset(buffer, 0, SIZE);
+    /*if (words == 5 && && strcmp(op, "REG") == 0) {
+        if (strcmp(user->uid, uid) == 0) {
+            sprintf(buffer, "RVC OK\n");
+            sendMessage(sfd, buffer, strlen(buffer));
+        } else {
+            sprintf(buffer, "RVC NOK\n");
+            sendMessage(sfd, buffer, strlen(buffer));
+        }
+    } else {
+        sprintf(buffer, "ERR\n");
+        sendMessage(sfd, buffer, strlen(buffer));
+    }*/
 }
 
 void getTCPrequests() {
@@ -42,7 +65,7 @@ void processCommands() {
         }
 
         if (FD_ISSET(sfdUDP->fd, &readfds)) {
-            getUDPrequests();
+            getUDPrequests(sfdUDP);
         }
 
         if (FD_ISSET(sfdTCP->fd, &readfds)) {
